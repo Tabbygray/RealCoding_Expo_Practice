59910aa5330ea03747ea48ac26bf868adc583302
201702000 <jundol1222@naver.com>
2021-05-24 23:57:48 +0900

    initial commit(working on step 09)

--- App.js
+++ App.js
@@ -1,14 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class CounterButton extends React.Component {
+  state = {
+    counter: 0,
+  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: props.counter,
+    };
+  }
+  render() {
+
+    const clickHandler = () => {
+      this.setState({
+        counter: this.state.counter + 1,
+      });
+      console.log("systest");
+    };
+
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.counter}</Text>
+        <Button title={'click me!'} onPress={clickHandler}/>
+      </View>
+    );
+
+  }
+
+}
+
+export default class App extends React.Component {
+  render() {
+
+    return (
+      <View style={styles.container}>
+        <CounterButton />
+        <CounterButton />
+        <CounterButton />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
